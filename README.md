653db596b8ed9b67b83772a4cb9f22e6aaba6eea
srhthu <shuirh2019@gmail.com>
2025-11-12 19:38:03 +0800

    Update three supported tasks

--- README.md
+++ README.md
@@ -1,10 +1,58 @@
 # ACL-Anthology-Crawler
 A toolkit to automatically crawl the paper list and download paper pdfs of ACL Anthology (https://aclanthology.org).
 
-## parameters
-- page_url: the url of the conference page of ACL Anthology.
-- conf_name: customized folder name.
-- conf_id: the html element id of the paper list block, e.g., `2020-acl-main`. Try to find the element id in the Chrome DevTools (F12). 
+## Usage
+
+```Bash
+python crawl.py -h
+usage: crawl.py [-h] [--url URL] [--sec_ids SEC_IDS [SEC_IDS ...]] [--save_path SAVE_PATH] task
+
+positional arguments:
+  task                  What to do: show: show sub section ids; get_info: get paper info; download: download all papers.
+
+options:
+  -h, --help            show this help message and exit
+  --url URL             The URL of the ACL Anthology conference page to crawl, e.g., https://aclanthology.org/events/emnlp-2025/
+  --sec_ids SEC_IDS [SEC_IDS ...]
+                        The IDs of the sub sections to process. If not provided, only the first section will be processed.
+  --save_path SAVE_PATH
+                        The path to save the output JSON file. Default to current directory.
+```
+
+### Show Sub-section IDs
+
+Each conference may have main conference papers, findings, workshops... You can list all these sub sections by
+
+```Bash
+python crawl.py show --url https://aclanthology.org/events/emnlp-2025/
+```
+
+The output is:
+```Text
+Section ID: 2025emnlp-main, Title: Proceedings of the 2025 Conference on Empirical Methods in Natural Language Processing
+Section ID: 2025emnlp-demos, Title: Proceedings of the 2025 Conference on Empirical Methods in Natural Language Processing: System Demonstrations
+Section ID: 2025emnlp-industry, Title: Proceedings of the 2025 Conference on Empirical Methods in Natural Language Processing: Industry Track
+Section ID: 2025emnlp-tutorials, Title: Proceedings of the 2025 Conference on Empirical Methods in Natural Language Processing: Tutorial Abstracts
+Section ID: 2025findings-emnlp, Title: Findings of the Association for Computational Linguistics: EMNLP 2025
+Section ID: 2025arabicnlp-main, Title: Proceedings of The Third Arabic Natural Language Processing Conference
+Section ID: 2025arabicnlp-sharedtasks, Title: Proceedings of The Third Arabic Natural Language Processing Conference: Shared Tasks
+...
+```
+
+### Get Paper List of One (or more) Sub-Sections
+
+For example, you can get the paper list (title, pdf_url) of the main conference of EMNLP 2025 by
+
+```Bash
+python crawl.py get_info --url https://aclanthology.org/events/emnlp-2025/ --sec_ids 2025emnlp-main
+# if you don't specify sec_ids, it will default to download the first section, which is usually the main conference.
+```
+
+The output file's each line is a JSON string:
+```json
+{"title": "Proceedings of the 2025 Conference on Empirical Methods in Natural Language Processing", "url": "https://aclanthology.org/2025.emnlp-main.0/", "pdf_url": "https://aclanthology.org/2025.emnlp-main.0.pdf", "section_id": "2025emnlp-main"}
+```
+
 
 ## Run
 ```
--- crawl.py
+++ crawl.py
@@ -6,40 +6,112 @@ import requests
 import os
 from tqdm import tqdm
 
-page_url = "https://aclanthology.org/events/acl-2020"
-conf_name = 'acl_2020_main'
-conf_id = '2020-acl-main'
-
-#%%
-html_doc = requests.get(page_url).text
-soup = BeautifulSoup(html_doc, 'html.parser')
-# %%
-main_papers = soup.find('div', id = conf_id).find_all('p', class_ = "d-sm-flex")
+# page_url = "https://aclanthology.org/events/emnlp-2025/"
+# conf_id = '2025emnlp-main'
 
 # %%
-paper_list = []
-for paper_p in main_papers:
-    pdf_url = paper_p.contents[0].contents[0]['href']
-    paper_span = paper_p.contents[-1]
-    assert paper_span.name == 'span'
-    paper_a = paper_span.strong.a
-    title = paper_a.get_text()
-    url = "https://aclanthology.org" + paper_a['href']
-    paper_list.append([title, url, pdf_url])
+def get_bs_soup_from_url(url):
+    """Given a URL, return the BeautifulSoup object of the HTML content."""
+    html_doc = requests.get(url).text
+    return BeautifulSoup(html_doc, 'html.parser')
 
+def get_main_section(html_soup):
+    """Return the main section containing multiple sub sections."""
+    return html_soup.find(id = 'main')
+
+def get_sub_sections(main_section):
+    """
+    Get a list of sub sections in the main section.
+    Each sub section is a "div" tag with an "id" attribute.
+
+    Return a list of tuples: (sub_section_id, sub_section_title)
+    """
+    sub_sections = []
+    for div in main_section.find_all('div', recursive = False):
+        if 'id' in div.attrs:
+            sub_section_id = div['id']
+            sub_section_title = find_title(div)
+            sub_sections.append((sub_section_id, sub_section_title))
+    return sub_sections
+
+def find_title(soup_obj):
+    """
+    Find the title of the sub section by looking the h tag with class d-sm-flex. Return its text.
+    """
+    h_tags = soup_obj.find_all(['h2', 'h3', 'h4', 'h5', 'h6'], class_ = 'd-sm-flex')
+    for h in h_tags:
+        # Find all "a" tag, whose class do not contain "badge"
+        for a in h.find_all('a'):
+            if 'badge' not in a.get('class', []):
+                return a.get_text().strip()
+    return "Unknown Section Title"
+def find_paper_list(soup, section_id):
+    """
+    Given the soup object and section id, return the list of paper entries in that section.
+    Each paper entry is a "p" tag with class "d-sm-flex".
+    """
+    paper_list = []
+    section_div = soup.find('div', id = section_id)
+    for paper_p in section_div.find_all('p', class_ = "d-sm-flex"):
+        pdf_url = paper_p.contents[0].contents[0]['href']
+        paper_span = paper_p.contents[-1]
+        assert paper_span.name == 'span'
+        paper_a = paper_span.strong.a
+        title = paper_a.get_text()
+        url = "https://aclanthology.org" + paper_a['href']
+        paper_list.append(
+            {"title": title, "url": url, "pdf_url": pdf_url}
+        )
+    
+    return paper_list
+
+def save_papers(paper_list, conf_name):
+    if not os.path.exists(conf_name):
+        os.mkdir(conf_name)
+
+    illegal_chr = r'\/:*?<>|'
+    table = ''.maketrans('', '', illegal_chr)
+    for i, paper in tqdm(list(enumerate(paper_list))):
+        r = requests.get(paper["pdf_url"])
+        n = '{}.{}.pdf'.format(i+1, paper["title"].translate(table))
+        with open('./{}/{}'.format(conf_name, n), 'wb') as f:
+            f.write(r.content)
 # %%
-with open(conf_name + '.json', 'w', encoding='utf8') as f:
-    json.dump(paper_list, f, indent = 2, ensure_ascii= False)
+if __name__ == "__main__":
+    import argparse
 
-print('There are total {} papers'.format(len(paper_list)))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('task', help = 'What to do: show: show sub section ids; get_info: get paper info; download: download all papers.')
+    parser.add_argument('--url', type=str, help='The URL of the ACL Anthology conference page to crawl, e.g., https://aclanthology.org/events/emnlp-2025/')
+    parser.add_argument('--sec_ids', type=str, nargs='+', default=None, help='The IDs of the sub sections to process. If not provided, only the first section will be processed.')
+    
+    parser.add_argument('--save_path', type=str, help='The path to save the output JSON file. Default to current directory.')
+    parser.add_argument('--save_dir', type=str, help='The path to save the pdf papers. Default to "./paper_pdf"', default = './paper_pdf')
+    args = parser.parse_args()
 
-if not os.path.exists(conf_name):
-    os.mkdir(conf_name)
+    if args.url is None:
+        raise ValueError("Please provide the URL of the ACL Anthology conference page to crawl using --url argument.")
+    soup = get_bs_soup_from_url(args.url)
+    main_section = get_main_section(soup)
+    sub_sections = get_sub_sections(main_section)
 
-illegal_chr = r'\/:*?<>|'
-table = ''.maketrans('', '', illegal_chr)
-for i, paper in tqdm(list(enumerate(paper_list))):
-    r = requests.get(paper[2])
-    n = '{}.{}.pdf'.format(i+1, paper[0].translate(table))
-    with open('./{}/{}'.format(conf_name, n), 'wb') as f:
-        f.write(r.content)
+    if args.task == 'show':
+        for sec_id, sec_title in sub_sections:
+            print(f"Section ID: {sec_id}, Title: {sec_title}")
+    else:
+        all_papers = []
+        target_sec_ids = args.sec_ids if args.sec_ids is not None else [sub_sections[0][0]]
+        for sec_id in target_sec_ids:
+            papers = find_paper_list(soup, sec_id)
+            for paper in papers:
+                paper['section_id'] = sec_id
+            all_papers.extend(papers)
+        print('Total papers found:', len(all_papers))
+        
+        if args.task == 'get_info':
+            save_path = args.save_path if args.save_path is not None else 'paper_list_{}.jsonl'.format(args.url.strip('/').split('/')[-1])
+            with open(save_path, 'w', encoding='utf-8') as f:
+                for row in all_papers:
+                    f.write(json.dumps(row, ensure_ascii=False) + '\n')
+        elif args.task == 'download':
+            save_papers(all_papers, args.save_dir)
